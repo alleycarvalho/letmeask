0f190ba5229b5cc0c4eac150aba51e7471ba7256
Alley M. Carvalho <alleycarvalho@gmail.com>
2021-06-25 00:01:16 -0300

    removal of questions

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
 
 import '../styles/room.scss'
 
@@ -9,6 +10,7 @@ import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
 // import { useAuth } from '../hooks/useAuth'
 import { useRoom } from '../hooks/useRoom'
+import { database } from '../services/firebase'
 
 type RoomParams = {
   id: string
@@ -20,6 +22,12 @@ export function AdminRoom() {
   const roomId = params.id
 
   const { questions, title } = useRoom(roomId)
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+    }
+  }
 
   return (
     <div id="page-room">
@@ -49,7 +57,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             )
           })}
         </div>
